084d64cac6501e421901311e1ffb97019234992a
Aleks Gisvold <gisvold@gmail.com>
2013-03-13 11:23:48 +0100

    Add ATCParser

--- src/parser/ATCParser.java
+++ src/parser/ATCParser.java
@@ -1,5 +1,76 @@
 package parser;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import dataClasses.ATC;
+
+
 public class ATCParser {
+	
+	private ArrayList<ATC> ATCClasses;
+	
+	public ATCParser(String ATCPath){
+		
+		ATCClasses = new ArrayList<ATC>();
+		
+		try{
+			
+			FileInputStream fileStream = new FileInputStream(ATCPath);
+			DataInputStream in = new DataInputStream(fileStream);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			
+			String ATCLine;
+			String[] tempArray = new String[5];
+			
+			while((ATCLine = br.readLine()) != null){
+				String[] parsed = codeParser(ATCLine);
+				for(int i = 0; i < tempArray.length; i++){
+					if(i > parsed.length-1){
+						tempArray[i]="";
+						
+					} else{
+						tempArray[i] = parsed[i];
+					}
+					
+					String label = labelParser(ATCLine);
+					ATC tempATC = new ATC(tempArray, label);
+					ATCClasses.add(tempATC);
+				}
+				
+				in.close();
+			}
+			
+		}catch(Exception e){
+				System.err.println("Error: " + e.getMessage());
+		}
+	}
+	
+	
+	public static String[] codeParser(String prologLine){
+		
+		String[] resultArray = new String[5];
+		String[] ATCCodeArray = prologLine.split("\\[");
+		ATCCodeArray = ATCCodeArray[1].split("\\]");
+		ATCCodeArray = ATCCodeArray[0].split(",");
+		
+		return ATCCodeArray;
+	}
+	
+	
+	public static String labelParser(String prologLine){
+		
+		String[] tempArray = prologLine.split("'");
+		
+		return tempArray[1];
+	}
+	
+	
+	public ArrayList<ATC> getATCClasses(){
+		return ATCClasses;
+	}
 
 }
